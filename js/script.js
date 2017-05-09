100900c6b48663331a3279e8198fae9bd714ee51
Zach Passarelli <zachary.passarelli@gmail.com>
2017-05-09 12:52:03 -0600

    another nameless commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
 $(document).ready(function(){
 
   var canvasReady = false;
+  var ctx = document.getElementById('grid').getContext('2d');
+  var cty = document.getElementById('grid2').getContext('2d');
+  var ctz = document.getElementById('grid3').getContext('2d');
+  var raf;
+
+  var note_img = new Image();
+  note_img.src = './media/note.png';
+  var hit_img = new Image();
+  hit_img.src = './media/hit.png';
+
+  var Note = function(x){
+    this.x = x;
+    this.y = 0;
+    this.img = note_img;
+    this.velocity = 5;
+  };
+
+  Note.prototype.drawme = function(){
+    cty.drawImage(this.img,this.x,this.y);
+  };
+  var note1 = new Note(5);
 
   function createGrid(){
     $('.info').fadeOut(500);
@@ -10,8 +31,6 @@ $(document).ready(function(){
       $('#pause').slideDown('slow');
     },500);
 
-    var canvas = document.getElementById('grid');
-    var ctx = canvas.getContext('2d');
     ctx.fillStyle = 'cyan';
     ctx.fillRect(50,0,3,500);
     ctx.fillRect(150,0,3,500);
@@ -42,17 +61,24 @@ $(document).ready(function(){
     ctx.strokeText('F',320,460);
 
     canvasReady = true;
+  }
 
+  function animate() {
+    cty.clearRect(0,0,400,500);
+    note1.drawme();
+    note1.y += note1.velocity;
+
+    raf = window.requestAnimationFrame(animate);
   }
 
   function pause(){
     var pauseState = $('#pause');
     if(pauseState.html() === 'play_arrow'){
+      window.cancelAnimationFrame(raf);
       pauseState.html('pause');
-      //pause game
     } else {
+      raf = window.requestAnimationFrame(animate);
       pauseState.html('play_arrow');
-      //resume game
     }
   }
 
@@ -64,18 +90,55 @@ $(document).ready(function(){
 
     createGrid();
 
-    // var test = "./media/rules.mp3";
-    // window.setTimeout(function(){
-    //   $('body').prepend('<audio src='+test+' autoplay></audio>');
-    // },5000);
+    var test;
+    // test = "./media/rules.mp3";
+    window.setTimeout(function(){
+      $('body').prepend('<audio src='+test+' autoplay></audio>');
+    },5000);
   });
 
-  $('body').keydown(function(event){
-    if(canvasReady){
-      if(event.keyCode === 32){ //spacebar
-        pause();
+    $(window).keydown(function(event){
+      if(canvasReady){
+        if(event.keyCode == '32'){ //spacebar
+          event.preventDefault();
+          pause();
+        }
+        if(event.keyCode == '65'){ //A
+          window.clearInterval(time1);
+          ctz.clearRect(0,430,100,30);
+          ctz.drawImage(hit_img,-5,430,110,30);
+          var time1 = window.setTimeout(function(){
+            ctz.clearRect(0,430,100,30);
+          },200);
+        }
+        if(event.keyCode == '83'){ //S
+          window.clearInterval(time2);
+          ctz.clearRect(100,430,100,30);
+          ctz.drawImage(hit_img,95,430,110,30);
+          var time2 = window.setTimeout(function(){
+            ctz.clearRect(100,430,100,30);
+          },200);
+        }
+        if(event.keyCode == '68'){ //D
+          window.clearInterval(time3);
+          ctz.clearRect(200,430,100,30);
+          ctz.drawImage(hit_img,195,430,110,30);
+          var time3 = window.setTimeout(function(){
+            ctz.clearRect(200,430,100,30);
+          },200);
+        }
+        if(event.keyCode == '70'){ //F
+          window.clearInterval(time4);
+          ctz.clearRect(300,430,100,30);
+          ctz.drawImage(hit_img,295,430,110,30);
+          var time4 = window.setTimeout(function(){
+            ctz.clearRect(300,430,100,30);
+          },200);
+        }
       }
-    }
-  });
+    });
+    window.setTimeout(function(){
+      animate();
+    },5000);
 
 });
